2e2f7e7c91be5b518d99cc3fc5a80377642c2f7d
ajmpc24 <ajmpc24@gmail.com>
2022-05-12 22:06:11 -0300

    update

--- Alura Challenge - Oracle ONE/Encriptador de Texto/script.js
+++ Alura Challenge - Oracle ONE/Encriptador de Texto/script.js
@@ -1,17 +1,23 @@
+const inputMensaje = document.querySelector(".form_input");
+const inputResultado = document.querySelector(".input-text-resultado");
 
-const form_input = document.querySelector(".form_input");
-const mensaje = document.querySelector("input-text-resultado");
+const botonEncriptar = document.querySelector("#boton1");
+const botonDesencriptar = document.querySelector("#boton2");
+const botonDesencriptar = (textoEncriptado);
 
-function botonEncriptar(){
-    const textoEncriptado = encriptar(form_input.value);
-    mensaje.value = textoEncriptado;
-}
 
-function encriptar(palabraParaEncriptar){
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["u", "ufat"]]
+function encriptar(palabraParaEncriptar) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["u", "ufat"]
+    ]
     palabraParaEncriptar = palabraParaEncriptar.toLowerCase();
-    for (let i=0; i<=matrizCodigo.length; i++){
-        if(palabraParaEncriptar.includes(matrizCodigo[i][0])){
+
+    for (let i = 0; i <= matrizCodigo.length; i++) {
+
+        if (palabraParaEncriptar.includes(matrizCodigo[i][0])) {
             palabraParaEncriptar = palabraParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
         }
     }
@@ -20,4 +26,4 @@ function encriptar(palabraParaEncriptar){
 
 
 
-    // botonDesencriptar()
+// botonDesencriptar()

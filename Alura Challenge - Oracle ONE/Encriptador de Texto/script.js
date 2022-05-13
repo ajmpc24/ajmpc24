e1befd4e39c6f4ef86f40d1b67e9988a6e1f1531
ajmpc24 <ajmpc24@gmail.com>
2022-05-12 23:20:20 -0300

    update

--- Alura Challenge - Oracle ONE/Encriptador de Texto/script.js
+++ Alura Challenge - Oracle ONE/Encriptador de Texto/script.js
@@ -1,29 +1,44 @@
-const inputMensaje = document.querySelector(".form_input");
-const inputResultado = document.querySelector(".input-text-resultado");
 
-const botonEncriptar = document.querySelector("#boton1");
-const botonDesencriptar = document.querySelector("#boton2");
-const botonDesencriptar = (textoEncriptado);
-
-
-function encriptar(palabraParaEncriptar) {
-    let matrizCodigo = [
-        ["e", "enter"],
-        ["i", "imes"],
-        ["a", "ai"],
-        ["u", "ufat"]
-    ]
-    palabraParaEncriptar = palabraParaEncriptar.toLowerCase();
-
-    for (let i = 0; i <= matrizCodigo.length; i++) {
-
-        if (palabraParaEncriptar.includes(matrizCodigo[i][0])) {
-            palabraParaEncriptar = palabraParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+const inputTexto= document.querySelector(".form_input");
+const mensaje= document.querySelector("#input-text-resultado");
+function botonEncriptar(){
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+}
+function encriptar(stringParaEncriptar) {
+    let matrizEncriptar =[["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringParaEncriptar = stringParaEncriptar.toLowerCase();
+    for (let i=0; i<matrizEncriptar.length; i++){
+        if (stringParaEncriptar.includes(matrizEncriptar[i][0])){
+            stringParaEncriptar = stringParaEncriptar.replaceAll(matrizEncriptar[i][0],matrizEncriptar[i][1])
         }
     }
+    return stringParaEncriptar;
+}
 
+function botonDesencriptar(){
+    const textoDesencriptado = encriptar(inputTexto.value);
+    mensaje.value = textoDesencriptado;
+}
+function desencriptar(stringParaDesencriptar) {
+    let matrizDesencriptar =[["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+    stringParaDesencriptar = stringParaDesencriptar.toLowerCase();
+    for (let i=0; i<matrizDesencriptar.length; i++){
+        if (stringParaDesencriptar.includes(matrizDesencriptar[i][1])){
+            stringParaDesencriptar = stringParaDesencriptar.replaceAll(matrizDesencriptar[i][1],matrizDesencriptar[i][0])
+        }
+    }
+    return stringParaDesencriptar;
 }
 
 
+/*
+function copiar() {
+    var Copiar = document.getElementById("form_input");
 
-// botonDesencriptar()
+    Copiar.select();
+    document.execCommand('copy');
+    alert("Copiado con éxito");
+    
+}
+*/

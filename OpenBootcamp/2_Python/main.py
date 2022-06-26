1bad1032868ce7239748ec00afb1dbd445a8bf15
ajmpc24 <ajmpc24@gmail.com>
2022-06-25 23:25:03 -0300

    ejercicios python

---
+++ OpenBootcamp/2_Python/main.py
@@ -0,0 +1,8 @@
+# Control de flujo
+# Ejercicio 1
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+edad = 14
+if edad > 18:
+    print("Es mayor de edad!")
+else:
+    print("Es menor de edad!")
